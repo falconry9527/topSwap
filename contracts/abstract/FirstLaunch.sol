da84eec42b8d7446a85cdf1a47f31a639cbe2f79
conry <kequan888@gmail.com>
2025-11-18 14:45:01 +0800

    fix

--- contracts/abstract/FirstLaunch.sol
+++ contracts/abstract/FirstLaunch.sol
@@ -5,7 +5,7 @@ abstract contract FirstLaunch {
     uint40 public launchedAtTimestamp;
 
     function launch() internal {
-        // require(launchedAtTimestamp == 0, "Already launched");
+        require(launchedAtTimestamp == 0, "Already launched");
         launchedAtTimestamp = uint40(block.timestamp);
     }
 }
