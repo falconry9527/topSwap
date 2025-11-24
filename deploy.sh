4ae3ec649f002a6a1b814baf1027b9c841df5681
conry <kequan888@gmail.com>
2025-11-24 15:18:10 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 npx hardhat clean
 npx hardhat compile
-npx hardhat run scripts/deploy_bsc.js --network bscTestnet
-npx hardhat run scripts/deploy_verify.js --network bscTestnet
+npx hardhat run scripts/deploy_bsc.js --network bscMainnet
+npx hardhat run scripts/deploy_verify.js --network bscMainnet
