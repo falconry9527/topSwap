e73c947c6640573f60aec39f5b3ab5e718d62abd
conry <kequan888@gmail.com>
2025-12-04 20:57:27 +0800

    fix

--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@ npx hardhat compile
 npx hardhat run scripts/deploy_bsc.js --network bscTestnet
 npx hardhat run scripts/deploy_lp.js --network bscTestnet
 
-
 npx hardhat run scripts/top_presale.js --network bscTestnet
 npx hardhat run scripts/staking_presale.js --network bscTestnet
 
